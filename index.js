c1c9293ec50c0365f482ad92e017750c82a16306
rm0087 <michetti.ray@gmail.com>
2024-06-16 22:13:55 -0400

    array filter method lab complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+
+function findMatching(arrayDrivers, driverName){
+    return arrayDrivers.filter(driver => driver.toLowerCase() === driverName.toLowerCase());
+}
+
+function fuzzyMatch(arrayDrivers, driverName){
+    return arrayDrivers.filter(driver => driver.toLowerCase().startsWith(driverName.toLowerCase()));
+}
+
+function matchName(arrayDrivers, driverName){
+    return arrayDrivers.filter(driver => driver.name.toLowerCase().startsWith(driverName.toLowerCase()));
+}
+
+
+
+
+
+
+
+
+
+
+
+
